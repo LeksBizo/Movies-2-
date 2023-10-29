7196fe530036d8400f7bbd80799f080303a98265
Leks <alexey.mishkel@gmail.com>
2023-10-29 18:59:09 +0300

    initial commit

---
+++ src/main/java/PosterManager.java
@@ -0,0 +1,10 @@
+public class PosterManager {
+    public Poster[] posters;
+
+    public void addPoster(Poster poster){
+       //....
+    }
+    public Poster[] getPosters(){
+        //.....
+    }
+}
